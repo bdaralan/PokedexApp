1cce74cfb5a739e7fcdf0c7f776767a11cd7775c
Dara <bd.daralan@yahoo.com>
2017-05-09 09:20:48 -0700

    added ability and tm detail VC

---
+++ PokedexApp/AbilityDetailVC.swift
@@ -0,0 +1,18 @@
+//
+//  AbilityDetailVC.swift
+//  PokedexApp
+//
+//  Created by Dara on 5/9/17.
+//  Copyright © 2017 iDara09. All rights reserved.
+//
+
+import UIKit
+
+class AbilityDetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+     
+    }
+}
---
+++ PokedexApp/TMDetailVC.swift
@@ -0,0 +1,18 @@
+//
+//  TMDetailVC.swift
+//  PokedexApp
+//
+//  Created by Dara on 5/9/17.
+//  Copyright © 2017 iDara09. All rights reserved.
+//
+
+import UIKit
+
+class TMDetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+     
+    }
+}
