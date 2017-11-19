f68a0f5ba7aebb8010adeb6cc6b97a4706d57509
iDara09 <bd.daralan@yahoo.com>
2017-11-18 16:00:32 -0800

    read and decoded allPokemons and allMegaEvolutionPokemons added pokemon.json and pokemon-mega-evolution.json

---
+++ PokedexApp/DBPokemon.swift
@@ -0,0 +1,88 @@
+//
+//  DBPokemon.swift
+//  PokedexApp
+//
+//  Created by Dara Beng on 11/17/17.
+//  Copyright © 2017 iDara09. All rights reserved.
+//
+
+import Foundation
+
+// NOTE:
+// This model is base on pokemon.json file exactly.
+// Must update the model accordingly as the json file change, or consider optional value.
+
+
+// MARK - Main class
+
+/// Pokemon
+/// - note: `Comparable` using only `id` and `name`
+///  - LessThan: compares `"\(id)\(name)"`
+///  - Equal: compare `id && name`
+public class DBPokemon: Encodable, Decodable, Comparable {
+    
+    let info: PokeInfo
+    let types: PokeType
+    let abilities: PokeAbility
+    let stats: PokeStat
+    let measurements: PokeMeasurement
+    
+    public static func <(lhs: DBPokemon, rhs: DBPokemon) -> Bool {
+        return "\(lhs.info.id)\(lhs.info.name)" < "\(rhs.info.id)\(rhs.info.name)"
+    }
+    
+    public static func ==(lhs: DBPokemon, rhs: DBPokemon) -> Bool {
+        let isEqualId = lhs.info.id == rhs.info.id
+        let isEqualName = lhs.info.name == rhs.info.name
+        return isEqualId && isEqualName
+    }
+}
+
+// MARK: - Attribute classes
+
+/// Pokemon info
+public struct PokeInfo: Encodable, Decodable {
+    
+    let id: Int
+    let name: String
+    let form: String
+    let evolutionTree: Int
+    let megaEvolution: [String]
+}
+
+
+/// Pokemon abilities
+public struct PokeAbility: Encodable, Decodable {
+    
+    let first: String
+    let second: String
+    let hidden: String
+}
+
+
+/// Pokemon types
+public struct PokeType: Encodable, Decodable {
+    
+    let primary: String
+    let secondary: String
+}
+
+
+/// Pokemon stats
+public struct PokeStat: Encodable, Decodable {
+    
+    let hp: Int
+    let attack: Int
+    let defense: Int
+    let spAttack: Int
+    let spDefense: Int
+    let speed: Int
+}
+
+
+/// Pokemon measurements
+public struct PokeMeasurement: Encodable, Decodable {
+    
+    let height: String
+    let weight: String
+}
---
+++ PokedexApp/PokeData.swift
@@ -0,0 +1,116 @@
+//
+//  PokeData.swift
+//  PokedexApp
+//
+//  Created by Dara Beng on 11/17/17.
+//  Copyright © 2017 iDara09. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - File name
+
+private var pokemonJsonFileName: String { return "pokemon" }
+
+private var pokemonMegaEvolutionJsonFileName: String { return "pokemon-mega-evolution" }
+
+
+// MARK: - Singleton
+
+private var _allPokemonsJson: DictionarySA!
+
+private var _allMegaEvolutionPokemonsJson: DictionarySA!
+
+private var _allPokemons: [DBPokemon]!
+
+private var _allMegaEvolutionPokemons: [DBPokemon]!
+
+
+// MARK: - Main class
+
+public struct PokeData {
+    
+    // MARK: - Getter
+    
+    /// All Pokemon with their information,
+    public static var allPokemon: [DBPokemon] { return _allPokemons }
+    
+    // MARK: - Function
+    
+    /// Initializes and prepares `PokeData`'s properties.
+    public static func initializes() {
+        _allPokemonsJson = readPokemonJson()
+        _allMegaEvolutionPokemonsJson = readMegaEvolutionPokemonJson()
+        _allPokemons = decodePokemons(from: _allPokemonsJson)
+        _allMegaEvolutionPokemons = decodePokemons(from: _allMegaEvolutionPokemonsJson)
+//        // TEST CODE, MUST DELETE WHEN DONE
+//        var dictionary = DictionarySA()
+//        let encoder = JSONEncoder()
+//        do {
+//            for pokemon in _allMegaEvolutionPokemons {
+//                let key = String.init(format: "%04d", pokemon.info.id) + pokemon.info.name
+//                let pokemonData = try encoder.encode(pokemon)
+//                let pokemonValue = try JSONSerialization.jsonObject(with: pokemonData, options: []) as AnyObject
+//                dictionary.updateValue(pokemonValue, forKey: key)
+//            }
+//            if #available(iOS 11, *) {
+//                let data = try JSONSerialization.data(withJSONObject: dictionary, options: .sortedKeys)
+//                let document = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
+//                let url = document!.appendingPathComponent("pokemon-mega-evolution").appendingPathExtension("json")
+//                try data.write(to: url, options: .atomic)
+//                print(url.path)
+//            }
+//        } catch { print(error.localizedDescription) }
+    }
+    
+    /// Read `pokemonJsonFileName.json` to `_allPokemonsJson`.
+    private static func readPokemonJson() -> DictionarySA {
+        return readJson(fileName: pokemonJsonFileName)
+    }
+    
+    /// Read `pokemonMegaEvolutionJsonFileName.json` to `_allPokemonMegaEvolutionJson`
+    private static func readMegaEvolutionPokemonJson() -> DictionarySA {
+        return readJson(fileName: pokemonMegaEvolutionJsonFileName)
+    }
+    
+    /// Read json file from main bundle.
+    /// - parameter fileName: json file name located in main bundle.
+    /// - returns: an empty `DictionarySA` if file not found.
+    private static func readJson(fileName: String) -> DictionarySA {
+        guard let path = Bundle.main.path(forResource: fileName, ofType: "json") else {
+            print("File: \(fileName).json is missing or cannot be read!!")
+            return [:]
+        }
+        do {
+            let url = URL(fileURLWithPath: path)
+            let data = try Data(contentsOf: url)
+            if let json = try JSONSerialization.jsonObject(with: data, options: []) as? DictionarySA {
+                return json
+            }
+        } catch {
+            print("readJson() fail to fetch \(fileName).json!!")
+            print(error.localizedDescription)
+        }
+        return [:]
+    }
+    
+    /// Decode every Pokemon from `_allPokemonsJson` as `Pokemon`.
+    /// Append every `Pokemon` to `_allPokemons`.
+    /// - parameter dictionary: `Dictionary` contains Pokemon data that can be decode.
+    private static func decodePokemons(from dictionary: DictionarySA) -> [DBPokemon] {
+        do {
+            var allPokemons = [DBPokemon]()
+            let decoder = JSONDecoder()
+            for (_, pokemonDict) in dictionary {
+                let pokemonData = try JSONSerialization.data(withJSONObject: pokemonDict, options: [])
+                let pokemon = try decoder.decode(DBPokemon.self, from: pokemonData)
+                allPokemons.append(pokemon)
+            }
+            return allPokemons
+        } catch {
+            print("decodeAllPokemons() fail!!")
+            print(error.localizedDescription)
+            return []
+        }
+    }
+}
